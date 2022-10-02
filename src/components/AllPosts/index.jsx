544dadbdc9d73dc49383bea95a7ce6c898dc28cc
Andrey <yuzlov2003@gmail.com>
2022-10-02 19:01:11 +0300

    upd8 - fix position cards

--- src/components/AllPosts/index.jsx
+++ src/components/AllPosts/index.jsx
@@ -1,11 +1,9 @@
 import React, {useContext, useState, useEffect} from "react";
-import Post from "../Post";
 import "./style.css";
 import posts from "../../data/posts.json";
 import Api from "../../../api";
 import { Ctx } from "../../App";
 import axios from "axios";
-import pic1 from "../../pictest.png"
 export default () => {
       const {api} = useContext(Ctx);
 
@@ -38,16 +36,18 @@ const [apiData, setApiData] = useState([])
       <input type="text" name="text" className="search" placeholder="Найти пост"/>
       <input type="submit" name="submit" className="submit" value="Найти" />
       
-      
+      <div className="mmain">
         {apiData.map((data) => {
           return ( <div className="pcard">
           <p className="ptitle">{data.title}</p>
+          <p className="ptitle">{data.text}</p>
           <img src={data.image} alt="pic" width="256px"/>
           </div>
           )
         })}
 
       {/* <Post data={posts} /> */}
+    </div>
     </div>
   );
 };
